50eb72ee7b3e674c48c1c9799e537f5e3b031d35
Igor Chakrygin <igor@chakrygin.ru>
2019-03-10 21:03:16 +0300

    add solution

---
+++ sandbox/AppSettings.Sandbox/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace AppSettings.Sandbox
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
